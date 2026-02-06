511d1ea10a1deb7f2e6990393173050380bd4d86
Youcef Dabouz <youcefdabouz@MacBook.local>
2026-02-06 16:46:55 +0100

    Remove unnecessary files

--- course/math.js
+++
@@ -1,9 +0,0 @@
-const add = (a, b) => (a + b);
-const sub = (a, b) => (a - b);
-const div = (a, b) => (a / b);
-const mul = (a, b) => (a * b);
-
-
-export {mul};
-
-//This is a module, we it's like a class, but it can hold multiple funs and vars, export for exporting
